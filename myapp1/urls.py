c90d59169901c784b1bdb53da6c448c10ae68ad2
prajal patidar <69393777+prajalpatidar06@users.noreply.github.com>
2021-06-06 15:26:20 +0530

    Update urls.py

--- myapp1/urls.py
+++ myapp1/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index , name='index'),
-    path('about', views.about , name='about'),
+    path('user_view', views.user_view , name='user_view'),
     path('register', views.user_register , name='register'),
     path('_login', views.user_login, name='_login'),
     path('logout', views.user_logout, name='logut'),
